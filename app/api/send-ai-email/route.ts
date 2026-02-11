71f5cd2270a1169ead6008bbbee7b2de67205a94
Hrishikesh-Kali <hrishikesh.kali@gmail.com>
2026-02-11 19:35:40 +0530

    Corrected email function error

--- app/api/send-ai-email/route.ts
+++ app/api/send-ai-email/route.ts
@@ -7,8 +7,12 @@ export async function POST(req: Request) {
     try{
         const body = await req.json();
         const {email} = body;
+    } catch (error) {
+        console.error(error);
+        return new Response('Internal Server Error', { status: 500 });
     }
-}
+};
+
 resend.emails.send({
   from: 'onboarding@resend.dev',
   to: 'kalihrishikesh@gmail.com',
